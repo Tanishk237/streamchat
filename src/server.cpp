46b0ef760211c3a9faf730ab8550ee0da9d250ae
Tanishk Varshney <tanishkvar@gmail.com>
2025-09-08 20:52:58 +0530

    server setup updated

--- src/server.cpp
+++ src/server.cpp
@@ -2,11 +2,24 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
-
+#include <unordered_map>
+#include <memory>
 
 using boost::asio::ip::tcp;
 
 std::mutex console_mutex;
+std::mutex clients_mutex;
+
+struct ClientInfo {
+    tcp::socket socket;
+    std::string username;
+
+    ClientInfo(tcp::socket&& sock, const std::string& user)
+        : socket(std::move(sock)), username(user) {}
+};
+
+std::unordered_map<int, std::shared_ptr<ClientInfo>> clients;
+int next_client_id = 0;
 
 tcp::acceptor start_acceptor(boost::asio::io_context& io_context, const std::string& ip, unsigned short port) {
     tcp::endpoint endpoint(boost::asio::ip::make_address(ip), port);
@@ -17,32 +30,78 @@ tcp::acceptor start_acceptor(boost::asio::io_context& io_context, const std::str
     return acceptor;
 }
 
+void broadcast_message(const std::string& message, int sender_id) {
+    std::lock_guard<std::mutex> lock(clients_mutex);
+
+    for (auto& [id, client] : clients) {
+        if (id != sender_id) {
+            boost::asio::write(client->socket, boost::asio::buffer(message));
+        }
+    }
+}
+
+std::string read_username(tcp::socket& socket) {
+    boost::asio::streambuf buf;
+    boost::asio::read_until(socket, buf, "\n");
+    std::istream is(&buf);
+    std::string username;
+    std::getline(is, username);
+    return username;
+}
 
 void client_session(tcp::socket socket) {
-    {
-        std::lock_guard<std::mutex> lock(console_mutex);
-        std::cout << "Client connected from " << socket.remote_endpoint() << std::endl;
-    }
-
     try {
+        std::string username = read_username(socket);
+
+        int client_id;
+        {
+            std::lock_guard<std::mutex> lock(clients_mutex);
+            client_id = next_client_id++;
+            clients[client_id] = std::make_shared<ClientInfo>(std::move(socket), username);
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(console_mutex);
+            std::cout << "User '" << username << "' connected from " << clients[client_id]->socket.remote_endpoint() << std::endl;
+        }
+
+        auto& client_socket = clients[client_id]->socket;
+
         for (;;) {
             char data[512];
             boost::system::error_code ec;
-            size_t length = socket.read_some(boost::asio::buffer(data), ec);
+            size_t length = client_socket.read_some(boost::asio::buffer(data), ec);
 
             if (ec == boost::asio::error::eof) {
-                break;
+                break; 
             } else if (ec) {
                 throw boost::system::system_error(ec);
             }
 
-            // Echo back the data (optional here, just for testing)
-            boost::asio::write(socket, boost::asio::buffer(data, length));
+            std::string message(data, length);
+
+            if (message == "/bye\n" || message == "/bye\r\n" || message == "/bye") {
+                boost::asio::write(client_socket, boost::asio::buffer("Goodbye!\n"));
+                break;
+            }
+
+            std::string broadcast_text = username + ": " + message;
+            broadcast_message(broadcast_text, client_id);
         }
-    }
-    catch (std::exception& e) {
+
+        {
+            std::lock_guard<std::mutex> lock(clients_mutex);
+            clients.erase(client_id);
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(console_mutex);
+            std::cout << "User '" << username << "' disconnected." << std::endl;
+        }
+
+    } catch (std::exception& e) {
         std::lock_guard<std::mutex> lock(console_mutex);
-        std::cout << "Client session error: " << e.what() << std::endl;
+        std::cout << "Exception in client session: " << e.what() << std::endl;
     }
 }
 
@@ -65,7 +124,6 @@ int main() {
             tcp::socket socket(io_context);
             acceptor.accept(socket);
 
-            // Start a thread to handle this client
             std::thread(client_session, std::move(socket)).detach();
         }
 
